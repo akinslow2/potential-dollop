e12704161dde71603146bf2fd3ac108b2715abbb
jordanrk <jordanrk@stanford.edu>
2017-08-12 14:19:15 -0700

    Annual energy for lights, sqftage too

--- lighting.py
+++ lighting.py
@@ -40,9 +40,34 @@ def space_type_conversion(space_type):
 			return row[1]
 
 
+def fluorescent_lighting_watts(model_number):
+	in_range = True
+	watts = ""
+	for char in model_number:
+		if not char.isdigit() and in_range:
+			in_range = False
+			continue
+		elif not in_range and not char.isdigit():
+			return int(watts)
+		else:
+			watts += char
+
+#This might have to change to incorporate different instances of lighting in 
+#one room, but we will change that later
+def area_energy_calculation(num_lamps_per_fixture, num_fixtures, watts, area):
+	return float(watts * num_fixtures * num_lamps_per_fixture) / area
+
+
+def total_energy_calculation(num_lamps_per_fixture, num_fixtures, test_hours, hours_on, watts):
+	hours_per_year = float(hours_on) / test_hours * 8760
+	total_watts = watts * num_fixtures * num_lamps_per_fixture
+	return hours_per_year * total_watts
+
+
 def main():
 	
-	over_under_lamped(-, space_type_conversion(-), -)
+	watts = fluorescent_lighting_watts("F32T10")
+	#over_under_lamped(-, space_type_conversion(-), -)
 
 
 if __name__ == '__main__':
