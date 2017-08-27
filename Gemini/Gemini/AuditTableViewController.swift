72f3078b072d932f1ac71e367395efe427212f51
jordanrk <jordanrk@stanford.edu>
2017-08-26 21:22:02 -0700

    Added robustness to zone selection

--- Gemini/Gemini/AuditTableViewController.swift
+++ Gemini/Gemini/AuditTableViewController.swift
@@ -10,23 +10,16 @@ import UIKit
 
 class AuditTableViewController: UITableViewController {
     
-    var items = ["Select type of zone"]
-    let feature_types = ["Lighting", "Heating", "Ventilation", "Air Conditioning", "Convection Oven", "TV"] //Add more values here!
-    var selectedValue = ""
+    var items = Array<String>()
+    //let feature_types = ["Lighting", "Heating", "Ventilation", "Air Conditioning", "Convection Oven", "TV"] //Add more values here!
+    //var selectedValue = ""
     @IBOutlet var table: UITableView!
-    let lighting_items = ["Measured Lux", "Room Type", "Room Area", "Units in Lumens?"]
+    //let lighting_items = ["Measured Lux", "Room Type", "Room Area", "Units in Lumens?"]
     //let hvac_items = []
     //let room_items = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-                self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,23 +28,25 @@ class AuditTableViewController: UITableViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         
-        print(selectedValue)
-        
-        if selectedValue == "Lighting zone" {
-                        
-            items = lighting_items
-            
-        } else if selectedValue == "HVAC zone" {
-            
-            //items = hvac_items
-            
-        } else {
-            
-            //items = room_items
-            
-        }
-        
         table.reloadData()
+        
+//        print(selectedValue)
+//        
+//        if selectedValue == "Lighting zone" {
+//                        
+//            items = lighting_items
+//            
+//        } else if selectedValue == "HVAC zone" {
+//            
+//            //items = hvac_items
+//            
+//        } else {
+//            
+//            //items = room_items
+//            
+//        }
+//        
+//        table.reloadData()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -76,31 +71,25 @@ class AuditTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "toAuditDetail", sender: self)
-        
-    }
-    
 
     
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if items.count > 1 {
-            
-            if segue.identifier == "toAuditDetail" {
-                
-                let auditDetailViewController = segue.destination as! AuditDetailViewController
-                
-                auditDetailViewController.items = feature_types
-                
-            }
-            
-        }
-        
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//        if items.count > 1 {
+//            
+//            if segue.identifier == "toAuditDetail" {
+//                
+//                let auditDetailViewController = segue.destination as! AuditDetailViewController
+//                
+//                auditDetailViewController.items = feature_types
+//                
+//            }
+//            
+//        }
+//        
+//    }
 }
